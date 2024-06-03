0c329bb3e39168c147b7e5d19e06cd9c6a5057c3
Niklas Kjenstadbakk Brede <niklasbrede@outlook.com>
2024-06-03 14:07:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import arcpy
 import os
 import pathlib
+import time
 
 def gis():
     aprx = arcpy.mp.ArcGISProject(
@@ -15,16 +16,31 @@ def gis():
             lyt = aprx.listLayouts()[i]
             lyt.exportToJPEG(os.path.join(output_directory, 'Skogtype'), 300)
 
+def check_folder(path):
+    return len(os.listdir(path))
+
+def vent():
+    with st.spinner('Vent du no litt Magne...'):
+        while True:
+            file_count = check_folder(f'S:\\Felles\\SamferdselInfrastruktur\\skredmal_streamlit\\GIS\\eksporterte_kart')
+            if file_count >= 1:
+                time.sleep(5)
+                st.balloons()
+                break
+            else:
+                time.sleep(2)
+
+
+
 st.title('Streamlit + ArcGIS = SANT')
 
 
-if st.button('Hent kart fra arcgis'):
+if st.button('Hent kart fra arcgis', on_click=vent()):
     gis()
 
 if os.path.exists(f'S:\\Felles\\SamferdselInfrastruktur\\skredmal_streamlit\\GIS\\eksporterte_kart\\Skogtype.JPG'):
     with open(f'S:\\Felles\\SamferdselInfrastruktur\\skredmal_streamlit\\GIS\\eksporterte_kart\\Skogtype.JPG', 'rb') as file:
         st.download_button(
-            label="Last ned kartet",
+            label="Last ned kart",
             data=file,
-            file_name=f'S:\\Felles\\SamferdselInfrastruktur\\skredmal_streamlit\\GIS\\eksporterte_kart\\Skogtype.JPG'
-            on_click=st.balloons())
+            file_name=f'S:\\Felles\\SamferdselInfrastruktur\\skredmal_streamlit\\GIS\\eksporterte_kart\\Skogtype.JPG')
